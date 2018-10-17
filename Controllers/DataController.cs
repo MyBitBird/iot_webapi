7b4b106484b3573436b24aeafaf25a23dcd443c5
meisam <meisammalekzadeh@gmail.com>
2018-10-17 23:51:13 +0330

    insert data with url added

--- Controllers/DataController.cs
+++ Controllers/DataController.cs
@@ -46,8 +46,37 @@ namespace IOT.Controllers
 
             ServiceLogs log = _mapper.Map<ServiceLogs>(dto);
 
-            return Ok(new { _service.AddData(userId,log,dto.ServiceData,await _servicePropertiesService.GetValidPropertiesByServiceId(dto.ServiceId)).Id});
+            return Ok(new { _service.AddData(userId,
+                                                log,
+                                                dto.ServiceData,
+                                                await _servicePropertiesService.GetValidPropertiesByServiceId(dto.ServiceId)).Id});
             
+        }
+
+        [HttpPost("{serviceId}"), Authorize]
+        public async Task<IActionResult> AddDataWithQuery(Guid serviceId,[FromQuery]DateTime logDate,String data)
+        {
+            if(logDate<new DateTime(2000,01,01) || data==null || data.Equals("")) return BadRequest();
+            
+            Guid userId = Utility.GetCurrentUserID(User);
+            if (!await _servicesService.HaveUserAccess(userId, serviceId)) return Forbid();
+
+            List<ServiceDataDTO> serviceData = new List<ServiceDataDTO>();
+            String[] items= data.Split(',');
+            foreach(String item in items)
+            {
+                String[] value = item.Split(':');
+                if(value.Length!=2) continue;
+                serviceData.Add(new ServiceDataDTO(){Code=value[0],Data=value[1]});
+                
+            }
+            ServiceLogs logs=new ServiceLogs() { ServiceId = serviceId, LogDate = logDate };
+            logs = await _service.AddData(userId,
+                                           logs,
+                                           serviceData,
+                                           await _servicePropertiesService.GetValidPropertiesByServiceId(serviceId));
+            return Ok(new {logs.Id });
+
         }
     }
 
