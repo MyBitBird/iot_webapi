44a8c1d9f06fbf5e435bc9bc54a014accf5a1d0b
meisam <meisammalekzadeh@gmail.com>
2018-11-07 17:07:06 +0330

    add ID in service DTO

--- Validators/ServceValidator.cs
+++ Validators/ServceValidator.cs
@@ -8,6 +8,7 @@ namespace IOT.Validators
         public ServiceValidator()
         {
             RuleFor(x=>x.title).NotEmpty().NotNull();
+            RuleFor(x=>x.Id).Null();
             RuleFor(x=>x.ServiceProperties).NotEmpty().NotNull();
             RuleForEach(x=>x.ServiceProperties).SetValidator(new ServicePropertiesValidator());
         }
