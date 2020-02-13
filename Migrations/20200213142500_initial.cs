624c93ec173d87c3aafea8b5aa995352c73b81c6
meisam <meisammalekzadeh@gmail.com>
2020-02-13 18:11:00 +0330

    enable & initial Migration

---
+++ Migrations/20200213142500_initial.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IOT.Migrations
+{
+    public partial class initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false, defaultValueSql: "newid()"),
+                    Username = table.Column<string>(maxLength: 100, nullable: false),
+                    Password = table.Column<string>(maxLength: 100, nullable: false),
+                    Name = table.Column<string>(maxLength: 100, nullable: false),
+                    Family = table.Column<string>(maxLength: 100, nullable: false),
+                    Status = table.Column<short>(nullable: false),
+                    Type = table.Column<short>(nullable: false),
+                    RegisterDate = table.Column<DateTime>(nullable: false),
+                    ParentUserId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Services",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false, defaultValueSql: "newid()"),
+                    UserId = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(maxLength: 250, nullable: false),
+                    Status = table.Column<short>(nullable: false),
+                    RegisterDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Services", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Services_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServiceLogs",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false, defaultValueSql: "newid()"),
+                    ServiceId = table.Column<Guid>(nullable: false),
+                    UserId = table.Column<Guid>(nullable: false),
+                    LogDate = table.Column<DateTime>(nullable: false),
+                    RegisterDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiceLogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ServiceLogs_Services_ServiceId",
+                        column: x => x.ServiceId,
+                        principalTable: "Services",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ServiceLogs_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServiceProperties",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false, defaultValueSql: "newid()"),
+                    ServiceId = table.Column<Guid>(nullable: false),
+                    Title = table.Column<string>(maxLength: 250, nullable: false),
+                    Code = table.Column<string>(maxLength: 50, nullable: false),
+                    Deleted = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiceProperties", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ServiceProperties_Services_ServiceId",
+                        column: x => x.ServiceId,
+                        principalTable: "Services",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServiceUsers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false, defaultValueSql: "newid()"),
+                    UserId = table.Column<Guid>(nullable: false),
+                    ServiceId = table.Column<Guid>(nullable: false),
+                    Deleted = table.Column<bool>(nullable: false),
+                    RegisterDate = table.Column<DateTime>(nullable: false, defaultValueSql: "CURRENT_TIMESTAMP")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiceUsers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ServiceUsers_Services_ServiceId",
+                        column: x => x.ServiceId,
+                        principalTable: "Services",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                    table.ForeignKey(
+                        name: "FK_ServiceUsers_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ServiceData",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false, defaultValueSql: "newid()"),
+                    ServicePropertyId = table.Column<Guid>(nullable: false),
+                    Data = table.Column<string>(maxLength: 500, nullable: true),
+                    ServiceLogId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ServiceData", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ServiceData_ServiceLogs_ServiceLogId",
+                        column: x => x.ServiceLogId,
+                        principalTable: "ServiceLogs",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ServiceData_ServiceProperties_ServicePropertyId",
+                        column: x => x.ServicePropertyId,
+                        principalTable: "ServiceProperties",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiceData_ServiceLogId",
+                table: "ServiceData",
+                column: "ServiceLogId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiceData_ServicePropertyId",
+                table: "ServiceData",
+                column: "ServicePropertyId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiceLogs_ServiceId",
+                table: "ServiceLogs",
+                column: "ServiceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiceLogs_UserId",
+                table: "ServiceLogs",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiceProperties_ServiceId",
+                table: "ServiceProperties",
+                column: "ServiceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Services_UserId",
+                table: "Services",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiceUsers_ServiceId",
+                table: "ServiceUsers",
+                column: "ServiceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ServiceUsers_UserId",
+                table: "ServiceUsers",
+                column: "UserId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ServiceData");
+
+            migrationBuilder.DropTable(
+                name: "ServiceUsers");
+
+            migrationBuilder.DropTable(
+                name: "ServiceLogs");
+
+            migrationBuilder.DropTable(
+                name: "ServiceProperties");
+
+            migrationBuilder.DropTable(
+                name: "Services");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
