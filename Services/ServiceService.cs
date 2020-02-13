ac76c3810fdb37a732d8759fc35892918a3b4357
meisam <meisammalekzadeh@gmail.com>
2020-02-14 01:36:28 +0330

    Refactor - ServiceService cleaning smelly codes

--- Services/ServiceService.cs
+++ Services/ServiceService.cs
@@ -7,37 +7,41 @@ using IOT.Helper;
 using System.Threading.Tasks;
 using System.Collections.Generic;
 
-namespace IOT.Services{
+namespace IOT.Services
+{
 
     public class ServiceService
     {
-        IOTContext _context;
+        private readonly IOTContext _context;
 
         public ServiceService(IOTContext context)
         {
-            _context=context;
-
+            _context = context;
         }
-        public async Task<Models.Services> NewService(Models.Services service,Guid userId)
+        public async Task<Models.Services> NewService(Models.Services service, Guid userId)
         {
-            service.RegisterDate=DateTime.Now;
-            service.Status=MyEnums.ServiceStatus.Active;
-            service.UserId=userId;
+            service.RegisterDate = DateTime.Now;
+            service.Status = MyEnums.ServiceStatus.Active;
+            service.UserId = userId;
             await _context.Services.AddAsync(service);
             await _context.SaveChangesAsync();
             return service;
-            
         }
 
-        public async Task<bool> HaveUserAccess(Guid userId,Guid serviceId)
+        public async Task<bool> HaveUserAccess(Guid userId, Guid serviceId)
         {
-            return await _context.ServiceUsers.AnyAsync(x=>x.ServiceId == serviceId && x.UserId == userId && x.Deleted == false);
+            return await _context.ServiceUsers.AnyAsync(x => x.ServiceId == serviceId && x.UserId == userId && x.Deleted == false);
         }
 
-        public async Task<Models.Services> GetById(Guid id,Guid userId)
+        public async Task<Models.Services> GetById(Guid id, Guid userId)
         {
-            Models.Services service= await _context.Services.AsNoTracking().Include(i=>i.ServiceProperties).FirstOrDefaultAsync(x=>x.Id==id && x.UserId==userId && x.Status == MyEnums.ServiceStatus.Active);
-            service.ServiceProperties= service.ServiceProperties.Where(x=>x.Deleted==false).ToArray();
+            var service = await _context.Services.AsNoTracking()
+                .Include(i => i.ServiceProperties)
+                .FirstOrDefaultAsync(x => x.Id == id &&
+                                          x.UserId == userId &&
+                                          x.Status == MyEnums.ServiceStatus.Active);
+
+            service.ServiceProperties = service.ServiceProperties.Where(x => x.Deleted == false).ToArray();
             return service;
         }
 
@@ -46,56 +50,62 @@ namespace IOT.Services{
             return await _context.Services.Where(x => x.UserId == userId && x.Status == MyEnums.ServiceStatus.Active).ToArrayAsync();
         }
 
-        public async Task<Boolean> UpdateService(Guid id, Models.Services service,Guid userId)
+        public async Task<bool> UpdateService(Guid id, Models.Services service, Guid userId)
         {
-            Models.Services preService = await GetById(id,userId);
-            
-            if(preService==null) return false;
+            var preService = await GetById(id, userId);
+            if (preService == null)
+                return false;
+
+            var newProperties = UpdateExProperties(service, preService);
+            AddNewPropertiesToService(id, newProperties);
 
             preService.Title = service.Title;
-
-            List<ServiceProperties> newProperties = service.ServiceProperties.ToList();
-
-            List<ServiceProperties> oldProperties = preService.ServiceProperties.ToList();
-
-            preService.ServiceProperties = null;//becuase we will see stupid error in add new item : exception collection was of a fixed size entity framework
-
-            foreach(ServiceProperties property in oldProperties)  
-            {
-                ServiceProperties isPropertyExist = service.ServiceProperties.FirstOrDefault(x => x.Id == property.Id);
-                if(isPropertyExist!=null)
-                {
-                    property.Title = isPropertyExist.Title;
-                    property.Code = isPropertyExist.Code;
-                    newProperties.Remove(isPropertyExist);
-                } 
-                else 
-                    property.Deleted=true;
-                    
-               _context.ServiceProperties.Update(property);
-            }
-
             _context.Services.Update(preService);
-
-           foreach (ServiceProperties property in newProperties)
-            {
-                property.ServiceId = id;
-                _context.ServiceProperties.Add(property);
-              
-            }
-            
             await _context.SaveChangesAsync();
             return true;
 
         }
-
-        public async Task<Boolean> Delete(Guid id,Guid userId)
+        private IEnumerable<ServiceProperties> UpdateExProperties(Models.Services newService, Models.Services exService)
         {
-            Models.Services service = await  GetById(id,userId);
+            //replacing newService old properties with new ones
+            var newProperties = newService.ServiceProperties.ToList();
+            var oldProperties = exService.ServiceProperties.ToList();
+            exService.ServiceProperties = null;
+
+            foreach (var property in oldProperties)
+            {
+                var isPropertyExist = newService.ServiceProperties.FirstOrDefault(x => x.Id == property.Id);
+                if (isPropertyExist != null) //update old properties info
+                {
+                    property.Title = isPropertyExist.Title;
+                    property.Code = isPropertyExist.Code;
+                    newProperties.Remove(isPropertyExist);
+                }
+                else //remove oldProperties that doesnt exist in new properties
+                    property.Deleted = true;
+
+                _context.ServiceProperties.Update(property);
+            }
+
+            return newProperties;
+        }
+
+        private void AddNewPropertiesToService(Guid serviceId, IEnumerable<ServiceProperties> newProperties)
+        {
+            foreach (var property in newProperties)
+            {
+                property.ServiceId = serviceId;
+                _context.ServiceProperties.Add(property);
+            }
+        }
+
+        public async Task<bool> Delete(Guid id, Guid userId)
+        {
+            var service = await GetById(id, userId);
 
             if (service == null) return false;
 
-            service.Status=MyEnums.ServiceStatus.Deleted;
+            service.Status = MyEnums.ServiceStatus.Deleted;
             _context.Services.Update(service);
             await _context.SaveChangesAsync();
             return true;
