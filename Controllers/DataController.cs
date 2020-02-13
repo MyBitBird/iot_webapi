700d8398b6400e3e4db17d71a67d9cf38e025d02
meisam <meisammalekzadeh@gmail.com>
2020-02-13 23:35:09 +0330

    refactor - clean DataController Code - moveing userId to constructor

--- Controllers/DataController.cs
+++ Controllers/DataController.cs
@@ -19,10 +19,11 @@ namespace IOT.Controllers
 
     public class DataController : ControllerBase
     {
-        ServiceLogService _service;
-        ServiceService _servicesService;
-        ServicePropertiesService _servicePropertiesService;
-        IMapper _mapper;
+        private readonly ServiceLogService _service;
+        private readonly ServiceService _servicesService;
+        private readonly ServicePropertiesService _servicePropertiesService;
+        private readonly IMapper _mapper;
+        private readonly Guid _userId;
 
         public DataController(IOTContext context, IMapper mapper)
         {
@@ -30,6 +31,7 @@ namespace IOT.Controllers
             _servicesService = new ServiceService(context);
             _servicePropertiesService = new ServicePropertiesService(context);
             _mapper = mapper;
+            _userId = Utility.GetCurrentUserId(User);
 
         }
 
@@ -37,58 +39,72 @@ namespace IOT.Controllers
         [Authorize(Roles = "DEVICE")]
         public async Task<IActionResult> AddData([FromBody] ServiceLogDTO dto)
         {
-            if (!this.ModelState.IsValid)
-            {
+            if (!ModelState.IsValid)
                 return BadRequest();
-            }
 
-            Guid userId = Utility.GetCurrentUserId(User); 
-            if (!await _servicesService.HaveUserAccess(userId, dto.ServiceId)) return Forbid();
+            if (!await _servicesService.HaveUserAccess(_userId, dto.ServiceId))
+                return Forbid();
 
-            ServiceLogs log = _mapper.Map<ServiceLogs>(dto);
-            log = await _service.AddData(userId,
+            var log = _mapper.Map<ServiceLogs>(dto);
+            log = await _service.AddData(_userId,
                                         log,
                                         dto.ServiceData,
                                         await _servicePropertiesService.GetValidPropertiesByServiceId(dto.ServiceId));
             return Ok(log.Id);
-            
         }
 
         [HttpPost("{serviceId}")]
         [Authorize(Roles = "DEVICE")]
-        public async Task<IActionResult> AddDataWithQuery(Guid serviceId,[FromQuery]DateTime logDate,String data)
+        public async Task<IActionResult> AddDataWithQuery(Guid serviceId, [FromQuery]DateTime logDate, string data)
         {
-            if(logDate<new DateTime(2000,01,01) || data==null || data.Equals("")) return BadRequest();
-            
-            Guid userId = Utility.GetCurrentUserId(User);
-            if (!await _servicesService.HaveUserAccess(userId, serviceId)) return Forbid();
+            if (IsDataValid(logDate, data))
+                return BadRequest();
 
-            List<DeviceDataDTO> serviceData = new List<DeviceDataDTO>();
-            String[] items= data.Split(',');
-            foreach(String item in items)
-            {
-                String[] value = item.Split(':');
-                if(value.Length!=2) continue;
-                serviceData.Add(new DeviceDataDTO(){Code=value[0],Data=value[1]});
-                
-            }
-            ServiceLogs logs=new ServiceLogs() { ServiceId = serviceId, LogDate = logDate };
-            logs = await _service.AddData(userId,
+            if (!await _servicesService.HaveUserAccess(_userId, serviceId))
+                return Forbid();
+
+            var serviceData = DeserializeData(data);
+
+            var logs = new ServiceLogs() { ServiceId = serviceId, LogDate = logDate };
+            logs = await _service.AddData(_userId,
                                            logs,
                                            serviceData,
                                            await _servicePropertiesService.GetValidPropertiesByServiceId(serviceId));
-            return Ok(new {logs.Id });
+            return Ok(new { logs.Id });
 
+        }
+
+        private static List<DeviceDataDTO> DeserializeData(string data)
+        {
+            var serviceData = new List<DeviceDataDTO>();
+            var items = data.Split(',');
+
+            const int keyValueLenght = 2;
+            foreach (var item in items)
+            {
+                var value = item.Split(':');
+                if (value.Length != keyValueLenght)
+                    continue;
+                serviceData.Add(new DeviceDataDTO() { Code = value[0], Data = value[1] });
+            }
+
+            return serviceData;
+        }
+
+        private static bool IsDataValid(DateTime logDate, string data)
+        {
+            return logDate < new DateTime(2000, 01, 01) || data == null || data.Equals("");
         }
 
         [HttpGet("{serviceId}")]
         [Authorize(Roles = "ADMIN")]
-        public async Task<IActionResult> GetData(Guid serviceId,[FromQuery] FilteringParams.Data filter)
+        public async Task<IActionResult> GetData(Guid serviceId, [FromQuery] FilteringParams.Data filter)
         {
-            Models.Services service = await _servicesService.GetById(serviceId,Utility.GetCurrentUserId(User));
-            if(service == null) return Forbid();
+            var service = await _servicesService.GetById(serviceId, _userId);
+            if (service == null)
+                return Forbid();
 
-            return Ok(_mapper.Map<ServiceLogDTO[]>(await _service.GetData(serviceId, filter))); 
+            return Ok(_mapper.Map<ServiceLogDTO[]>(await _service.GetData(serviceId, filter)));
 
         }
     }
