be8a4900c4ff69ea3f9d8fb91692d22206a533e1
meisam <meisammalekzadeh@gmail.com>
2020-02-15 13:56:17 +0330

    add ServiceLog storage for solving external dependency access

---
+++ Storage/IServiceLogStorage.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using IOT.Models;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace IOT.Storage
+{
+    public interface IServiceLogStorage
+    {
+        Task<EntityEntry<ServiceLogs>> AddAsync(ServiceLogs log);
+
+        Task<EntityEntry<ServiceData>> AddServiceDataAsync(ServiceData data);
+
+        Task<int> SaveChangesAsync();
+
+        IQueryable<ServiceLogs> AsQueryable();
+
+    }
+}
---
+++ Storage/ServiceLogStorage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using IOT.Models;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace IOT.Storage
+{
+    public class ServiceLogStorage : IServiceLogStorage
+    {
+        private readonly IOTContext _context;
+        public ServiceLogStorage(IOTContext context)
+        {
+            _context = context;
+        }
+        public async Task<EntityEntry<ServiceLogs>> AddAsync(ServiceLogs log)
+        {
+            return await _context.ServiceLogs.AddAsync(log);
+        }
+
+        public async Task<EntityEntry<ServiceData>> AddServiceDataAsync(ServiceData data)
+        {
+            return await _context.ServiceData.AddAsync(data);
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+
+        public IQueryable<ServiceLogs> AsQueryable()
+        {
+            return _context.ServiceLogs.AsQueryable();
+        }
+    }
+}
